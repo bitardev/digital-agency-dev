08761f54e3da517d7fd2c3b1bcbf4046263ea620
Bitar Youssef <bitardev@gmail.com>
2024-02-05 13:48:51 +0100

    fix accordion not showing on loading

--- components/Accordion.tsx
+++ components/Accordion.tsx
@@ -25,7 +25,6 @@ const Accordion = ({
           data-aos-easing="ease-in-out"
           data-aos-mirror="true"
           data-aos-once="false"
-          data-aos-anchor-placement="top-bottom"
         >
           <button
             type="button"
